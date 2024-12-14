cbae2c28932928b9d818e58136ecbd4596dfd1b4
Ball <ballgo120@gmail.com>
2024-12-14 14:13:23 +0700

    fix: 🐛 update vegetable responsive

--- src/sections/components/vegetable/layout/Layout.tsx
+++ src/sections/components/vegetable/layout/Layout.tsx
@@ -5,7 +5,11 @@ interface Props {
 }
 
 const VegetableLayout: FC<Props> = ({ children }: Props) => {
-    return <div className="grid grid-cols-3 gap-10 px-8 pb-8">{children}</div>
+    return (
+        <div className="grid grid-cols-1 gap-10 px-8 pb-8 sm:grid-cols-3">
+            {children}
+        </div>
+    )
 }
 
 export default VegetableLayout
